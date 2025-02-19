177bafa0d47cd99034c49f1d8d380d91e675d481
Vagiz Akhmetov <vagmen27@gmail.com>
2025-02-19 14:46:43 +0300

    Версия 0.2.0

--- example/index.tsx
+++ example/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Thing } from '../src';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { HeroSlice } from "../src";
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <HeroSlice />
     </div>
   );
 };
 
-const root = createRoot(document.getElementById('root')!);
+const root = createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <App />
